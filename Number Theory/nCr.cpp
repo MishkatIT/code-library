c1d232e289c827265e568fc2e65d30dbf2e95274
MishkatIT <mishkatcse1@gmail.com>
2023-11-14 19:57:49 +0600

    mod value modified

--- Number Theory/nCr.cpp
+++ Number Theory/nCr.cpp
@@ -12,7 +12,7 @@ ll modAdd(ll x, ll y) { x %= mod, y %= mod; modNormalize(x), modNormalize(y); re
 ll modSubtract(ll x, ll y) { x %= mod, y %= mod; modNormalize(x), modNormalize(y); x -= y; modNormalize(x); return x; }
 
 ll modPower(ll base, ll exponent) { ll result = 1; while (exponent > 0) { if (exponent & 1) result = modMultiply(result, base); base = modMultiply(base, base); exponent >>= 1; } return result; }
-ll modInverse(ll x) { return modPower(x, mod - 2); }  /// if mod is prime.
+ll modInverse(ll x) { return modPower(x, mod - 2); }  // if mod is prime.
 ll modDivide(ll x, ll y) { return modMultiply(x, modInverse(y)); }
 
 ll fact[maxN + 1];
--- Number Theory/nPr.cpp
+++ Number Theory/nPr.cpp
@@ -9,7 +9,7 @@ void modNormalize(ll &x) { x %= mod; if (x < 0) x += mod; }
 
 ll modMultiply(ll x, ll y) { x %= mod, y %= mod; modNormalize(x), modNormalize(y); return (x * y) % mod; }
 ll modPower(ll base, ll exponent) { ll result = 1; while (exponent > 0) { if (exponent & 1) result = modMultiply(result, base); base = modMultiply(base, base); exponent >>= 1; } return result; }
-ll modInverse(ll x) { return modPower(x, mod - 2); }  /// if mod is prime.
+ll modInverse(ll x) { return modPower(x, mod - 2); }  // if mod is prime.
 
 ll fact[maxN + 1];
 
