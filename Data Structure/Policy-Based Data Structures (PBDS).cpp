7cc3131e27eb5fbbb9d7919cee76b4a0d471b2ae
MishkatIT <mishkatcse1@gmail.com>
2024-01-21 19:59:19 +0600

    re arranged

--- Data Structure/Policy-Based Data Structures (PBDS).cpp
+++ Data Structure/Policy-Based Data Structures (PBDS).cpp
@@ -1,10 +1,10 @@
 
 #include <bits/stdc++.h>
+using namespace std;
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-using namespace std;
-
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 int main() {
