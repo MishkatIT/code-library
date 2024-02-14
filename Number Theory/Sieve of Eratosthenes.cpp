ce2bc49abd34b52e2a71a338a75c8570f5280e18
MishkatIT <mishkatcse1@gmail.com>
2024-02-15 02:46:37 +0600

    sieve updated

--- Number Theory/Sieve of Eratosthenes.cpp
+++ Number Theory/Sieve of Eratosthenes.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 const int N = 2e6;
+
+
 vector<int>prime;
+vector<bool> isPrime(N, true);
 void sieve() {
-    vector<bool> isPrime(N, true);
     isPrime[0] = isPrime[1] = false;
-    
+
     for (int i = 2; i * i < N; i++) {
         if (isPrime[i]) {
             for (int j = i * i; j < N; j += i) {
@@ -21,6 +22,8 @@ void sieve() {
         }
     }
 }
+
+
 
 int main() {
     sieve();
